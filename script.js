ba39c8a9435324e862bc214a279047fe4fe3a35b
vladislavpetrov4311 <petrov_vd_4311@mail.ru>
2023-01-28 17:18:50 +0300

    в зависимости ОС выгружаем текст

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 
 var temp, temp2;
 
-function test()
+function test2()
  {
    
      temp = '<p><q>Инсктрукция PDF</q> <a href="Zayavlenie_dlya_bakalavrov.pdf" target="_blank">открыть в браузере</a>';
@@ -13,6 +13,37 @@ function test()
 document.getElementById("result").innerHTML = temp2;
 
 
+
+
+////////////////////////////////////////////////
+
+
+var userDeviceArray = [
+    {device: '<q1>Ваша ОС Android</q1>', platform: /Android/},
+    {device: '<q1>Ваша ОС iPhone</q1>', platform: /iPhone/},
+    {device: '<q1>Ваша ОС iPad</q1>', platform: /iPad/},
+    {device: '<q1>Ваша ОС Windows Phone</q1>', platform: /Windows Phone/},
+    {device: '<q1>Ваша ОС Linux</q1>', platform: /Linux/},
+    {device: '<q1>Ваша ОС Windows 10</q1>', platform: /Windows NT 10.0/}
+];
+
+var platform = navigator.userAgent;
+
+//просто для примера
+var T, q_ , q1_;
+T = '<q1>Более подробно</q1><p>';
+q_ = '<q1>';
+q1_ = '</q1>'
+
+    for (var i in userDeviceArray) {
+        if (userDeviceArray[i].platform.test(platform)) 
+        {
+            document.getElementById("result2").innerHTML = userDeviceArray[i].device;
+            document.getElementById("result3").innerHTML = T + q_ + platform + q1_;
+        }
+    }
+    return 'Неизвестная платформа!' + platform;
+    
 
 }
 
