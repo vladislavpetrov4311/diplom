c97003f5b7ea5856d1edba4b6b8305997fda281d
vladislavpetrov4311 <petrov_vd_4311@mail.ru>
2023-01-28 15:11:28 +0300

    сам скрипт

--- script.js
+++ script.js
@@ -1,32 +1,17 @@
 
 
 
-var A, A2, temp, temp2;
+var temp, temp2;
 
 function test()
  {
-    A = document.getElementById("name").value; //получаем данные из формы 
-    A2 = document.getElementById("name2").value;
-     temp = '<p>Инсктрукция PDF <a href="Zayavlenie_dlya_bakalavrov.pdf" target="_blank">открыть в браузере</a>';
-     temp2 = '<p>Файл для развёртывания ВМ на Вашей ОС <a href="2.rar" download>Скачать </a>';
+   
+     temp = '<p><q>Инсктрукция PDF</q> <a href="Zayavlenie_dlya_bakalavrov.pdf" target="_blank">открыть в браузере</a>';
+     temp2 = '<a href="2.rar" download="">Скачать</a>';
     
 
+document.getElementById("result").innerHTML = temp2;
 
-
-
-   if(A=='Astra Linux' && A2 =='свой ПК')
-   {
-document.getElementById("result").innerHTML = temp;
-//document.getElementById("result2").innerHTML = temp2;
-   
-   }
-
-if(A=='Windows XP' && A2 =='335')
-   {
-document.getElementById("result").innerHTML = temp;
-document.getElementById("result2").innerHTML = temp2;
-   }
- 
 
 
 }
